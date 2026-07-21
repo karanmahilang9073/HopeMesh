149413ff4103d6e5be6bb26073eebddcef0a7a2f
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-07-21 18:19:29 +0530

    backend(auth): all zod schema created

--- backend/src/validators/auth.validator.js
+++ backend/src/validators/auth.validator.js
@@ -27,3 +27,40 @@ export const loginSchema = z.object({
     password: z.string({required_error: 'password is required'})
         .min(1, 'password is required')
 }).strict()
+
+export const forgotPasswordSchema = z.object({
+    email: z.string({required_error: 'email is required'})
+        .trim().toLowerCase().email('invalid email')
+}).strict()
+
+export const resetPasswordSchema = z.object({
+    token: z.string({required_error: 'token is required'})
+        .trim().min(1, 'token is required'),
+    password: z.string({required_error: 'password is required'})
+        .min(6).max(20)
+        .regex(/[A-Z]/, 'password must contain atleast one upper case character')
+        .regex(/[a-z]/, 'password must contain atleast one lower case character')
+        .regex(/[0-9]/, 'password must contain atleast one number'),
+    confirmPassword: z.string({required_error: 'confirm password is required'})
+        .min(1, 'confirm password is required')
+}).strict().refine((data) => data.password === data.confirmPassword, {
+    message: 'password do not match',
+    path: ['confirmPassword'],
+})
+
+export const changePasswordSchema = z.object({
+    currPassword: z.string({required_error: 'current password is required'}).min(1, 'current password is required'),
+    newPassword: z.string({required_error: 'new Password is required'})
+        .min(6).max(20)
+        .regex(/[A-Z]/, 'password must contain at least one upper case character')
+        .regex(/[a-z]/, 'password must contain at least one lower case character')
+        .regex(/[0-9]/, 'password must contain at least one numeric value'),
+    confirmPassword: z.string({required_error: 'confirm password is required'})
+        .min(1, 'confirm password is required'),
+}).strict().refine((data) => data.newPassword === data.confirmPassword, {
+    message: 'password do not match with confirm password',
+    path: ['confirmPassword'],
+}).refine((data) => data.newPassword !== data.currPassword, {
+    message: 'new password must be different from current password',
+    path: ['newPassword']
+})
