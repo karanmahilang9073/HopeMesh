0d762151f1ad7ef4b016fc1ab0ad2f033b47eb55
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-07-22 21:04:24 +0530

    ApiError added in auth service

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,22 +1,21 @@
 import User from "../models/User.model.js"
 import crypto from 'crypto'
+import {ApiError} from '../utils/apiError.js'
 
 export const register =async(userData) => {
     const emailExist = await User.findOne({email: userData.email})
     if (emailExist) {
-        throw new Error('email already exists')
+        throw new ApiError(409, 'email already exists')
     }
     const phoneExist = await User.findOne({phone: userData.phone})
     if (phoneExist) {
-        throw new Error('phone number already exists')
+        throw new ApiError(409, 'phone number already exists')
     }
     const emailverificationToken = crypto.randomBytes(32).toString('hex')
     const emailVerificationExp = new Date(Date.now() + 15 * 60 * 1000)
 
     const user = await User.create({...userData, emailverificationToken, emailVerificationExp})
     return {
-        success: true,
-        message: 'registration successfull',
         user: {
             _id: user._id,
             fullname: user.fullname,
