d22fdd6ee1969ed38f97f9fe7804f6277307ddbc
Karan Mahilang <156161187+karanmahilang9073@users.noreply.github.com>
2026-07-22 19:45:18 +0530

    backend: error middleware iplemented

--- backend/src/App.js
+++ backend/src/App.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import limiter from './middleware/rateLimiter.js';
 import router from './routes/index.js';
+import { errorMiddleware } from './middleware/error.middleware.js';
 
 
 export const app = express();
@@ -20,6 +21,7 @@ app.use(express.urlencoded({extended: true}))
 
 app.use('/api/v1', router)
 
+app.use(errorMiddleware)
 
 app.get('/health', (req,res) => {
     res.json({success: true, message: 'HopeMesh: connect the donors to required places'})
